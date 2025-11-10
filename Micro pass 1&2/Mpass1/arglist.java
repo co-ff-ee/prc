7ddb4798d97f65a2dcec306ecbf59007f8931540
co-ff-ee <prasadgirigosavi22@gmail.com>
2025-11-11 00:23:24 +0530

    Add files via upload

---
+++ Micro pass 1&2/Mpass1/arglist.java
@@ -0,0 +1,9 @@
+public class arglist {
+    String argname;
+    String value;
+
+    arglist(String argname) {
+        this.argname = argname;
+        this.value = null; // Default value
+    }
+}
---
+++ Micro pass 1&2/Mpass2/MPass2.java
@@ -0,0 +1,169 @@
+import java.io.*;
+
+class mdt {
+    String stmnt;
+}
+
+class mnt {
+    String name;
+    int addr;
+    int arg_cnt;
+
+    mnt(String n, int a, int c) {
+        name = n;
+        addr = a;
+        arg_cnt = c;
+    }
+}
+
+class arglist {
+    String argname;
+    String value;
+
+    arglist(String n) {
+        argname = n;
+        value = "";
+    }
+}
+
+public class MPass2 {
+    public static void main(String[] args) throws IOException {
+        mdt[] MDT = new mdt[50];
+        mnt[] MNT = new mnt[20];
+        arglist[] formal_parameter = new arglist[20];
+        arglist[] actual_parameter = new arglist[20];
+
+        int macro_addr = -1;
+        boolean macro_start = false, macro_end = false;
+        int macro_call = -1;
+        int mdt_cnt = 0, mnt_cnt = 0, formal_arglist_cnt = 0,
+                actual_arglist_cnt = 0;
+
+        // ------------------ READ MNT ------------------
+        BufferedReader br1 = new BufferedReader(new FileReader("MNT.txt"));
+        String line;
+        while ((line = br1.readLine()) != null) {
+            String[] parts = line.trim().split("\\s+");
+            if (parts.length < 3) continue;
+            MNT[mnt_cnt++] = new mnt(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
+        }
+        br1.close();
+
+        System.out.println("\n\t**********MACRO NAME TABLE***********");
+        System.out.println("\n\tINDEX\tNAME\tADDRESS\tARG_CNT");
+        for (int i = 0; i < mnt_cnt; i++) {
+            System.out.println("\t" + i + "\t" + MNT[i].name + "\t" + MNT[i].addr + "\t" + MNT[i].arg_cnt);
+        }
+
+        // ------------------ READ ARGLIST ------------------
+        br1 = new BufferedReader(new FileReader("ARGLIST.txt"));
+        while ((line = br1.readLine()) != null) {
+            String[] parameters = line.trim().split("\\s+");
+            if (parameters.length > 0) {
+                formal_parameter[formal_arglist_cnt] = new arglist(parameters[0]);
+                if (parameters.length > 1) {
+                    formal_parameter[formal_arglist_cnt].value = parameters[1];
+                }
+                formal_arglist_cnt++;
+            }
+        }
+        br1.close();
+
+        System.out.println("\n\n\t************FORMAL ARGUMENT LIST**************");
+        System.out.println("\n\tINDEX\tNAME\tVALUE");
+        for (int i = 0; i < formal_arglist_cnt; i++) {
+            System.out.println("\t" + i + "\t" + formal_parameter[i].argname + "\t" + formal_parameter[i].value);
+        }
+
+        // ------------------ READ MDT ------------------
+        br1 = new BufferedReader(new FileReader("MDT.txt"));
+        while ((line = br1.readLine()) != null) {
+            MDT[mdt_cnt] = new mdt();
+            MDT[mdt_cnt++].stmnt = line.trim();
+        }
+        br1.close();
+
+        System.out.println("\n\t***********MACRO DEFINITION TABLE**************");
+        System.out.println("\n\tINDEX\tSTATEMENT");
+        for (int i = 0; i < mdt_cnt; i++) {
+            System.out.println("\t" + i + "\t" + MDT[i].stmnt);
+        }
+
+        // ------------------ PROCESS INPUT ------------------
+        br1 = new BufferedReader(new FileReader("input.txt"));
+        BufferedWriter bw1 = new BufferedWriter(new FileWriter("output.txt"));
+
+        while ((line = br1.readLine()) != null) {
+            line = line.replaceAll(",", " ");
+            String[] tokens = line.trim().split("\\s+");
+
+            for (String current_token : tokens) {
+                if (current_token.equalsIgnoreCase("MACRO")) {
+                    macro_start = true;
+                    macro_end = false;
+                } else if (current_token.equalsIgnoreCase("MEND")) {
+                    macro_end = true;
+                    macro_start = false;
+                } else if (macro_end && !macro_start) {
+                    // check macro call
+                    for (int i = 0; i < mnt_cnt; i++) {
+                        if (current_token.equalsIgnoreCase(MNT[i].name)) {
+                            macro_call = i;
+                            actual_arglist_cnt = 0;
+
+                            // collect arguments
+                            for (int k = 1; k < tokens.length; k++) {
+                                if (tokens[k].contains("=")) {
+                                    String[] pair = tokens[k].split("=");
+                                    actual_parameter[actual_arglist_cnt++] = new arglist(pair[1]);
+                                } else {
+                                    actual_parameter[actual_arglist_cnt++] = new arglist(tokens[k]);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    if (macro_call == -1) {
+                        bw1.write(line + "\n"); // normal line
+                        break;
+                    }
+                }
+            }
+
+            // ------------------ MACRO EXPANSION ------------------
+            if (macro_call != -1) {
+                macro_addr = MNT[macro_call].addr + 1;
+                while (true) {
+                    if (MDT[macro_addr].stmnt.equalsIgnoreCase("MEND")) {
+                        macro_call = -1;
+                        break;
+                    } else {
+                        String[] temp_tokens = MDT[macro_addr++].stmnt.split("\\s+");
+                        for (String temp : temp_tokens) {
+                            if (temp.matches("#[0-9]+")) {
+                                int num = Integer.parseInt(temp.replaceAll("[^0-9]+", ""));
+                                // ✅ FIX: skip invalid #0 references
+                                if (num > 0 && actual_parameter[num - 1] != null) {
+                                    bw1.write(actual_parameter[num - 1].argname + "\t");
+                                }
+                            } else {
+                                bw1.write(temp + "\t");
+                            }
+                        }
+                        bw1.write("\n");
+                    }
+                }
+            }
+        }
+
+        br1.close();
+        bw1.close();
+
+        // ------------------ ACTUAL ARGUMENT LIST ------------------
+        System.out.println("\n\n\t*********ACTUAL ARGUMENT LIST**********");
+        System.out.println("\n\tINDEX\tNAME");
+        for (int i = 0; i < actual_arglist_cnt; i++) {
+            System.out.println("\t" + i + "\t" + actual_parameter[i].argname);
+        }
+    }
+}
